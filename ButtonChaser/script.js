ddae4fd86e840e9f78bea4e39116bb4430dfd653
Atavachron <vladimir.kalichuk@gmail.com>
2020-04-14 00:37:26 +0400

    Comments added

--- ButtonChaser/script.js
+++ ButtonChaser/script.js
@@ -11,15 +11,15 @@ let aWidth, aHeight, timer;
 window.addEventListener("load", setArea);
 
 function setArea() {
-  aWidth = innerWidth - 22;
-  aHeight = innerHeight - 97;
+  aWidth = innerWidth - 22; //Two by 10px (for each side) and two by 1px border
+  aHeight = innerHeight - 97; // The area is positioned75px  from the top + 22px from above (to make space equal at the top and the sides)
 
   gameArea.style.width = `${aWidth}px`;
   gameArea.style.height = `${aHeight}px`;
   dot.addEventListener("click", detectHit);
 
-  aWidth -= 74;
-  aHeight -= 74;
+  aWidth -= 64; //To make sure that the button stays within the area, its width and height are subtracted from the available width and height
+  aHeight -= 64; //This will not affect the gameArea width and height but will matter in the moveDot function that follows.
 
   moveDot();
 }
@@ -30,10 +30,10 @@ function detectHit() {
 }
 
 function moveDot() {
-  let x = Math.floor(Math.random() * aWidth);
+  let x = Math.floor(Math.random() * aWidth); //The aWidth/aHeight here are the adjusted ones, with the button dimensions subtracted
   let y = Math.floor(Math.random() * aHeight);
 
-  if (x < 10) x = 10;
+  if (x < 10) x = 10; //This is to prevent the top left corner of the button be over the top/left line
   if (y < 10) y = 10;
 
   dot.style.left = `${x}px`;
