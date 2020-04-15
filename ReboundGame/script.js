373b14e4adcb1752ea990bd6620c67a143b72f7b
Atavachron <vladimir.kalichuk@gmail.com>
2020-04-15 13:08:46 +0400

    Added code for paddle movement

--- ReboundGame/script.js
+++ ReboundGame/script.js
@@ -19,6 +19,7 @@ function init() {
   const ball = get("#ball");
   const paddle = get("#paddle");
   const score = get("#score");
+  document.addEventListener("keydown", keyListener);
   layoutPage();
 }
 
@@ -27,4 +28,20 @@ function layoutPage() {
   pHeight = innerHeight - 20;
   playingArea.style.width = `${pWidth}px`;
   playingArea.style.height = `${pHeight}px`;
+}
+
+function keyListener(e) {
+  let pressed = e.key;
+  if ((pressed == "ArrowLeft" || pressed == "a") && paddleLeft > 0) {
+    paddleLeft -= pdx;
+    if (paddleLeft < 0) paddleLeft = 0;
+    paddle.style.left = `${paddleLeft}px`;
+  } else if (
+    (pressed == "ArrowRight" || pressed == "d") &&
+    paddleLeft < pWidth - 64
+  ) {
+    paddleLeft += pdx;
+    if (paddleLeft > pWidth - 64) paddleLeft = pWidth - 64;
+  }
+  paddle.style.left = `${paddleLeft}px`;
 }
