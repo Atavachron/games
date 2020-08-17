948ccd11c14ca8b05b84fcfd97592e59630fc8d0
Atavachron <37329317+Atavachron@users.noreply.github.com>
2020-08-18 00:10:31 +0400

    Add event listener for arrow keys

--- PacMan/script.js
+++ PacMan/script.js
@@ -60,3 +60,35 @@ function createBoard() {
 }
 
 createBoard()
+
+//The starting position of PacMan 
+let pacmanCurrentIndex = 490;
+//Use the current position to add the class of PacMan to the element with this index
+squares[pacmanCurrentIndex].classList.add('pacman');
+
+function controlPacman(event) {
+  switch (event.key) {
+    case "Down": // IE/Edge specific value
+    case "ArrowDown":
+      // Do something for "down arrow" key press.
+      console.log("down")
+      break;
+    case "Up": // IE/Edge specific value
+    case "ArrowUp":
+      // Do something for "up arrow" key press.
+      console.log("up");
+      break;
+    case "Left": // IE/Edge specific value
+    case "ArrowLeft":
+      // Do something for "left arrow" key press.
+      console.log("left");
+      break;
+    case "Right": // IE/Edge specific value
+    case "ArrowRight":
+      // Do something for "right arrow" key press.
+      console.log("right")
+      break;
+  }
+}
+
+document.addEventListener('keyup', controlPacman);
